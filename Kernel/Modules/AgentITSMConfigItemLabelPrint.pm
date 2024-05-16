c49629d651ff1c2523adf891e1735ef39890c7b7
Stefan Härter <sha@rother-oss.com>
2024-05-16 17:47:54 +0200

    Reworked the package based on PDF::API2.045.

---
+++ Kernel/Modules/AgentITSMConfigItemLabelPrint.pm
@@ -0,0 +1,391 @@
+# --
+# OTOBO is a web-based ticketing system for service organisations.
+# --
+# Kernel/Modules/AgentITSMConfigItemLabelPrint.pm - print layout for itsm config item agent interface
+# Copyright (C) 2019-2024 Rother OSS GmbH, https://otobo.de/
+# --
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# --
+
+package Kernel::Modules::AgentITSMConfigItemLabelPrint;
+
+use strict;
+use warnings;
+
+# core modules
+
+# CPAN modules
+
+# OTOBO modules
+use Kernel::Language qw(Translatable);
+
+our $ObjectManagerDisabled = 1;
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    # set default values for mm
+    $Self->{mm} = 25.4 / 72;
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # get needed objects
+    my $ConfigItemObject = $Kernel::OM->Get('Kernel::System::ITSMConfigItem');
+    my $ConfigObject     = $Kernel::OM->Get('Kernel::Config');
+    my $LayoutObject     = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
+    my $ParamObject      = $Kernel::OM->Get('Kernel::System::Web::Request');
+    my $PDFObject        = $Kernel::OM->Get('Kernel::System::PDF');
+
+    # get params
+    my $ConfigItemID = $ParamObject->GetParam( Param => 'ConfigItemID' );
+    my $VersionID    = $ParamObject->GetParam( Param => 'VersionID' );
+
+    # check needed stuff
+    if ( !$ConfigItemID || !$VersionID ) {
+        return $LayoutObject->ErrorScreen(
+            Message => 'No ConfigItemID or VersionID is given!',
+            Comment => 'Please contact the admin.',
+        );
+    }
+
+    # get config of frontend module
+    $Self->{Config} = $ConfigObject->Get("ITSMConfigItem::Frontend::$Self->{Action}");
+
+    # get config item by version id
+    my $ConfigItem = $ConfigItemObject->ConfigItemGet(
+        VersionID     => $VersionID,
+        DynamicFields => 1,
+    );
+
+    # check if config item exists
+    if ( !$ConfigItem->{ConfigItemID} ) {
+        return $LayoutObject->ErrorScreen(
+            Message => "No config item found for config item id $ConfigItemID and version id $VersionID!",
+            Comment => 'Please contact the admin.',
+        );
+    }
+
+    # check if config item id and version id from request refer to same config item
+    if ( $ConfigItem->{ConfigItemID} != $ConfigItemID ) {
+        if ( !$ConfigItem->{ConfigItemID} ) {
+            return $LayoutObject->ErrorScreen(
+                Message => "Invalid request!",
+                Comment => 'Please contact the admin.',
+            );
+        }
+    }
+
+    # check for access rights
+    my $HasAccess = $ConfigItemObject->Permission(
+        Scope  => 'Item',
+        ItemID => $ConfigItem->{ConfigItemID},
+        UserID => $Self->{UserID},
+        Type   => $Self->{Config}{Permission},
+    );
+
+    if ( !$HasAccess ) {
+
+        # error page
+        return $LayoutObject->ErrorScreen(
+            Message => 'Can\'t show config item, no access rights given!',
+            Comment => 'Please contact the admin.',
+        );
+    }
+
+    # prepare PDF
+    my %Page = (
+        Width           => $Self->{Config}{PageSetting}{PageWidth} / $Self->{mm},     # (optional) default 595 (Din A4) - _ both or nothing
+        Height          => $Self->{Config}{PageSetting}{PageHeight} / $Self->{mm},    # (optional) default 842 (Din A4) -
+        PageOrientation => $Self->{Config}{PageSetting}{PageOrientation},             # (optional) default normal (normal|landscape)
+        MarginTop       => $Self->{Config}{PageSetting}{MarginTop},                   # (optional) default 0 -
+        MarginRight     => $Self->{Config}{PageSetting}{MarginRight},                 # (optional) default 0  |_ all or nothing
+        MarginBottom    => $Self->{Config}{PageSetting}{MarginBottom},                # (optional) default 0  |
+        MarginLeft      => $Self->{Config}{PageSetting}{MarginLeft},                  # (optional) default 0 -
+        ShowPageNumber  => 0,                                                         # (optional) default 1
+    );
+
+    # create new pdf document
+    $PDFObject->DocumentNew(
+        Encode => $LayoutObject->{UserCharset},
+    );
+
+    # create first pdf page
+    $PDFObject->PageBlankNew(
+        %Page,
+    );
+
+    # output general information
+    $Self->_PDFOutputGeneralInfos(
+        Page       => \%Page,
+        ConfigItem => $ConfigItem,
+    );
+
+    my $PDFString = $PDFObject->DocumentOutput();
+
+    # get current timestamp for filename
+    my $DateTimeObject = $Kernel::OM->Create('Kernel::System::DateTime');
+    if ( $Self->{UserTimeZone} ) {
+        $DateTimeObject->ToTimeZone( TimeZone => $Self->{UserTimeZone} );
+    }
+
+    # create filename
+    my $Filename = 'configitem_' . $ConfigItem->{Number} . '_';
+    $Filename .= $DateTimeObject->Format( Format => '%Y-%m-%d_%H:%M' );
+    $Filename .= '.pdf';
+    my $CleanedFilename = $Kernel::OM->Get('Kernel::System::Main')->FilenameCleanUp(
+        Filename => $Filename,
+        Type     => 'Attachment',
+    );
+
+    my %AttachmentData = (
+        Filename    => $CleanedFilename,
+        ContentType => 'application/pdf',
+        Content     => $PDFString,
+        Disposition => 'attachment',
+    );
+
+    return $LayoutObject->Attachment(
+        %AttachmentData,
+    );
+}
+
+sub _PDFOutputGeneralInfos {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Argument (qw(Page ConfigItem)) {
+        if ( !defined $Param{$Argument} ) {
+            $Kernel::OM->Get('Kernel::System::Log')->Log(
+                Priority => 'error',
+                Message  => "Need $Argument!",
+            );
+            return;
+        }
+    }
+
+    # get needed objects
+    my $LayoutObject = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
+    my $PDFObject    = $Kernel::OM->Get('Kernel::System::PDF');
+
+    # get readable values for config item dynamic fields
+    my %ReadableConfigItemData;
+    for my $Key ( keys $Param{ConfigItem}->%* ) {
+
+        if ( $Key =~ /^DynamicField_(.+)$/ ) {
+
+            # get field config
+            my $FieldName          = $1;
+            my $DynamicFieldConfig = $Kernel::OM->Get('Kernel::System::DynamicField')->DynamicFieldGet(
+                Name => $FieldName,
+            );
+
+            # fetch and set value
+            my $ValueStrg = $Kernel::OM->Get('Kernel::System::DynamicField::Backend')->ReadableValueRender(
+                DynamicFieldConfig => $DynamicFieldConfig,
+                Value              => $Param{ConfigItem}{$Key},
+                ValueMaxChars      => 30,
+                TitleMaxChars      => 30,
+            );
+            $ReadableConfigItemData{$Key} = $ValueStrg->{Value};
+        }
+        else {
+            $ReadableConfigItemData{$Key} = $Param{ConfigItem}{$Key};
+        }
+    }
+
+    # print Logo
+    if ( $Self->{Config}{LogoSetting} ) {
+
+        # check if file exists
+        my $Home = $Kernel::OM->Get('Kernel::Config')->Get('Home');
+        my $Path = "$Home/$Self->{Config}{LogoSetting}{LogoPath}";
+        if ( -e $Path ) {
+
+            $PDFObject->PositionSet(
+                X => $Self->{Config}{LogoSetting}{ImagePosition_X},
+                Y => $Self->{Config}{LogoSetting}{ImagePosition_Y},
+            );
+
+            my %Image = (
+                File   => $Path,
+                Type   => $Self->{Config}{LogoSetting}{DisplayType},
+                Width  => $Self->{Config}{LogoSetting}{ImageWidth},
+                Height => $Self->{Config}{LogoSetting}{ImageHeight},
+            );
+
+            $PDFObject->Image(%Image);
+        }
+    }
+
+    # print Table
+    if ( $Self->{Config}{TableSetting} ) {
+
+        $PDFObject->PositionSet(
+            X => $Self->{Config}{TableSetting}{TablePosition_X},
+            Y => $Self->{Config}{TableSetting}{TablePosition_Y},
+        );
+
+        # collect text to print
+        my @TextData;
+
+        if ( $Self->{Config}{Text} ) {
+            for my $TextLine ( $Self->{Config}{Text}->@* ) {
+
+                push @TextData, $LayoutObject->Output(
+                    Template => $TextLine,
+                    Data     => \%ReadableConfigItemData,
+                );
+            }
+        }
+
+        my %TableParam;
+
+        $TableParam{CellData}[0][0]{Font}     = 'ProportionalBold';
+        $TableParam{CellData}[0][0]{FontSize} = 11;
+        $TableParam{CellData}[0][0]{Align}    = 'center';
+        $TableParam{CellData}[0][0]{Content}  = $TextData[0];
+
+        VALUEROW:
+        for my $RowCount ( 1 .. $#TextData ) {
+            $TableParam{CellData}[$RowCount][0]{Font}     = 'Proportional';
+            $TableParam{CellData}[$RowCount][0]{FontSize} = 8;
+            $TableParam{CellData}[$RowCount][0]{Align}    = 'center';
+            $TableParam{CellData}[$RowCount][0]{Content}  = $TextData[$RowCount];
+        }
+
+        $TableParam{ColumnData}[0]{Width} = $Self->{Config}{TableSetting}{ValueWidth};
+        $TableParam{Type}                 = $Self->{Config}{TableSetting}{Type};
+        $TableParam{Border}               = $Self->{Config}{TableSetting}{Border};
+        $TableParam{FontSize}             = $Self->{Config}{TableSetting}{FontSize};
+        $TableParam{Padding}              = $Self->{Config}{TableSetting}{Padding};
+        $TableParam{PaddingTop}           = $Self->{Config}{TableSetting}{PaddingTop};
+        $TableParam{PaddingBottom}        = $Self->{Config}{TableSetting}{PaddingBottom};
+        $TableParam{PaddingLeft}          = $Self->{Config}{TableSetting}{PaddingLeft};
+        $TableParam{PaddingRight}         = $Self->{Config}{TableSetting}{PaddingRight};
+
+        # output table (or a fragment of it)
+        %TableParam = $PDFObject->Table(%TableParam);
+    }
+
+    # print HLine
+    if ( $Self->{Config}{HLineSetting} ) {
+
+        $PDFObject->PositionSet(
+            X => $Self->{Config}{HLineSetting}{HLinePosition_X},
+            Y => $Self->{Config}{HLineSetting}{HLinePosition_Y},
+        );
+
+        $PDFObject->HLine(
+            Color     => $Self->{Config}->{HLineSetting}->{Color},        # (optional) default black
+            LineWidth => $Self->{Config}->{HLineSetting}->{LineWidth},    # (optional) default 1
+        );
+
+    }
+
+    # Set window preferences
+    $PDFObject->PositionSet(
+        X => "left",
+        Y => "top",
+    );
+
+    # print Barcode
+    if ( $Self->{Config}{BarcodeSetting} ) {
+
+        my $Data = $Param{ConfigItem}{ $Self->{Config}{BarcodeSetting}{BarcodeField} };
+
+        # create link in case of QR code
+        if ( $Self->{Config}{BarcodeSetting}{Code} eq 'qr' ) {
+
+            if ( $Self->{Config}{BarcodeSetting}{QRCodeLink} =~ /ITSMConfigItemZoom$/ ) {
+
+                # get link parts
+                my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+                my $HTTPType     = $ConfigObject->Get('HttpType');
+                my $FQDN         = $ConfigObject->Get('FQDN');
+
+                $Data = "$HTTPType://$FQDN/otobo/index.pl?Action=$Self->{Config}{BarcodeSetting}{QRCodeLink};ConfigItemID=$Param{ConfigItem}{ConfigItemID}";
+            }
+            elsif ( $Self->{Config}{BarcodeSetting}{QRCodeLink} eq 'Custom' ) {
+                $Data = $Kernel::OM->Get('Kernel::Output::HTML::Layout')->Output(
+                    Template => $Self->{Config}{BarcodeSetting}{CustomLink},
+                    Data     => $Param{ConfigItem},
+                );
+            }
+        }
+
+        my %Options = (
+            'bar_height'   => $Self->{Config}{BarcodeSetting}{Zone}             || 10,
+            'bar_extend'   => $Self->{Config}{BarcodeSetting}{LowerMendingZone} || 15,
+            'font'         => $Self->{Config}{BarcodeSetting}{Font} ? $PDFObject->{PDF}->font( $Self->{Config}{BarcodeSetting}{Font} ) : undef,
+            'font_size'    => $Self->{Config}{BarcodeSetting}{FontSize}    || 8,
+            'bar_overflow' => $Self->{Config}{BarcodeSetting}{BarOverflow} || 0.5,
+        );
+
+        # be cautious and set caption only if wanted, because PDF::API2 checks with 'exists' at one point
+        if ( $Self->{Config}{BarcodeSetting}{Caption} ) {
+            $Options{caption} = $Param{ConfigItem}{ $Self->{Config}{BarcodeSetting}{BarcodeField} };
+        }
+        else {
+            delete $Options{caption};
+        }
+
+        # add barcode
+        my $Code = $PDFObject->{PDF}
+            ->barcode(
+                $Self->{Config}{BarcodeSetting}{Code},
+                $Data,
+                %Options,
+            );
+
+        $PDFObject->{PDF}->open_page(1)->object(
+            $Code,
+            $Self->{Config}{BarcodeSetting}{BarcodePosition_X},
+            $Self->{Config}{BarcodeSetting}{BarcodePosition_Y},
+            $Self->{Config}{BarcodeSetting}{Scale_X},
+            $Self->{Config}{BarcodeSetting}{Scale_Y},
+        );
+    }
+
+    # print Footer
+    if ( $Self->{Config}{FooterSetting} ) {
+
+        $PDFObject->PositionSet(
+            X => $Self->{Config}{FooterSetting}{FooterPosition_X},
+            Y => $Self->{Config}{FooterSetting}{FooterPosition_Y},
+        );
+
+        # evaluate FooterText
+        my $FooterText = $LayoutObject->Output(
+            Template => $Self->{Config}{FooterSetting}{FooterText},
+            Data     => \%ReadableConfigItemData,
+        );
+
+        $PDFObject->Text(
+            Text     => $FooterText,
+            Font     => $Self->{Config}->{FooterSetting}->{Font},
+            FontSize => $Self->{Config}->{FooterSetting}->{FontSize},
+            Color    => $Self->{Config}->{FooterSetting}->{Color},
+            Align    => $Self->{Config}->{FooterSetting}->{Align},
+            Lead     => $Self->{Config}->{FooterSetting}->{Lead},
+        );
+    }
+
+    return 1;
+}
+
+1;
--- doc/content/index.rst
+++ doc/content/index.rst
@@ -1,7 +1,3 @@
-.. image:: ../images/otobo-logo.png
-   :align: center
-|
-
 .. toctree::
     :maxdepth: 2
     :caption: Contents
@@ -22,7 +18,7 @@ OTOBO 11.0.x
 
 Packages
 --------
-\-
+ITSMConfigurationManagement 11.0.0
 
 Third-party software
 --------------------
@@ -30,9 +26,57 @@ Third-party software
 
 Usage
 =====
+The package ITSMConfigItemLabelPrint brings the functionality to create a highly customizable PDF file. This can be done via the added "LabelPrint" button in the Zoom view in the Agent interface.
 
 Setup
 -----
+To shape the resulting PDF file as needed, a wide variety of options is available. The PDF consists of 5 elements (see Picture) and the respective system configuration:
+
+- Logo (`ITSMConfigItem::Frontend::AgentITSMConfigItemLabelPrint###LogoSetting`)
+- Text (`ITSMConfigItem::Frontend::AgentITSMConfigItemLabelPrint###Text`)
+- Horizontal Line (`ITSMConfigItem::Frontend::AgentITSMConfigItemLabelPrint###HLineSetting`)
+- Barcode (`ITSMConfigItem::Frontend::AgentITSMConfigItemLabelPrint###BarcodeSetting`)
+- Footer (`ITSMConfigItem::Frontend::AgentITSMConfigItemLabelPrint###FooterSetting`)
+
+Each of this is only printed if the corresponding system configuration setting is active. This way, it can be defined which elements are wanted and which elements are omitted entirely.
+
+.. figure:: screenshot01.png
+   :scale: 70%
+   :alt: The sreenshot shows the elements available. 
+
+   The screenshot shows the elements available.
+
+Logo
+^^^^
+For the logo, an image file with the logo is needed. It can be placed in every location in the OTOBO directory. Recommended would be a location like `/opt/otobo/var/https/htdocs/skins/Agent/default/img/LogoFile.png`. The location then must be put into the system configuration setting `Kernel/Modules/AgentITSMConfigItemLabelPrint.pm`, key `LogoPath`. The setting holds some other options which can be used to customize the appearance of the logo in the PDF.
+
+Text
+^^^^
+The text is printed in a table-like structure, per default from the top middle position downwards. Each entry in the array of the system configuration setting `ITSMConfigItem::Frontend::AgentITSMConfigItemLabelPrint###Text` is printed as a separate line. Within the lines, Template::Toolkit syntax can be used to put in data from the configuration item, e.g. like 'Nr.: [% Data.Number %]', which will produce a result like 'Nr.: 123456789'.
+
+The styling and positioning options of the Text can be customized with the system configuration setting `ITSMConfigItem::Frontend::AgentITSMConfigItemLabelPrint###TableSetting`.
+
+Horizontal Line
+^^^^^^^^^^^^^^^
+For optical structure, a horizontal line can be placed at a customizable height. In the system configuration setting `ITSMConfigItem::Frontend::AgentITSMConfigItemLabelPrint###HLineSetting`, also the color and line width can be changed.
+
+Barcode
+^^^^^^^
+The system configuration setting `ITSMConfigItem::Frontend::AgentITSMConfigItemLabelPrint###BarcodeSetting` can be used to change format and appearance of the barcode. The settings have the following meaning:
+
+- BarcodeField: Attribute of the config item used for the barcode.
+- Caption: Whether to put the data below the barcode as caption. Ignored for QR code.
+- QRCodeLink: When creating a QR code, set the link destination.
+- CustomLink: Define a custom link via Template::Toolkit syntax for QR codes.
+- Code: Which type of code to print.
+- Zone: The base height of the barcode in points.
+- LowerMendingZone: If present and applicable, bars for non-printing characters (e.g. start and stop characters) will be extended downward by this many points, and printing characters will be shown below their respective bars. This is enabled by default for EAN-13 barcodes.
+- Font: Font to use for the barcode caption.
+- FontSize: Font size to use for the barcode caption.
+
+Footer
+^^^^^^
+In the system configuration setting `ITSMConfigItem::Frontend::AgentITSMConfigItemLabelPrint###FooterSetting`, location, appearance and content of a footer section can be specified. The key 'FooterText' is capable of interpreting Template::Toolkit syntax (compare description of the Text section).
 
 Configuration Reference
 -----------------------
@@ -65,52 +109,45 @@ Frontend::Navigation###AgentITSMConfigItemLabelPrint###003-AgentITSMConfigItemLa
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 Main menu item registration.
 
-Frontend::AgentITSMConfigItemLabelPrint
+Frontend::Agent::View::AgentITSMConfigItemLabelPrint
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-ITSMConfigItem::Frontend::AgentITSMConfigItemLabelPrint###LogoSetting
+ITSMConfigItem::Frontend::AgentITSMConfigItemLabelPrint###BarcodeSetting
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-Used logo settings for the label. The best size is 142 x 187 px. Please use here px.
+Used barcode settings for the label. Options have the following meaning:
+- BarcodeField: Attribute of the config item used for the barcode.
+- Caption: Whether to put the data below the barcode as caption. Ignored for QR code.
+- QRCodeLink: When creating a QR code, set the link destination.
+- CustomLink: Define a custom link via Template::Toolkit syntax for QR codes.
+- Code: Which type of code to print.
+- Zone: The base height of the barcode in points.
+- LowerMendingZone: If present and applicable, bars for non-printing characters (e.g. start and stop characters) will be extended downward by this many points, and printing characters will be shown below their respective bars. This is enabled by default for EAN-13 barcodes.
+- Font: Font to use for the barcode caption.
+- FontSize: Font size to use for the barcode caption.
 
 ITSMConfigItem::Frontend::AgentITSMConfigItemLabelPrint###TableSetting
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 Used table settings for the label.
 
-ITSMConfigItem::Frontend::AgentITSMConfigItemLabelPrint###BarcodeSetting
+ITSMConfigItem::Frontend::AgentITSMConfigItemLabelPrint###LogoSetting
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-Used barcode settings for the label.
-
-ITSMConfigItem::Frontend::AgentITSMConfigItemLabelPrint###FooterSetting
-""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-Used footer settings for the label.
+Used logo settings for the label. The best size is 142 x 187 px. Please use here px.
 
 ITSMConfigItem::Frontend::AgentITSMConfigItemLabelPrint###HLineSetting
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 Used settings for the horizont line.
 
-ITSMConfigItem::Frontend::AgentITSMConfigItemLabelPrint###Value2
+ITSMConfigItem::Frontend::AgentITSMConfigItemLabelPrint###Text
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-It´s possible to print up to 5 CI values to the label. Please add here the CI key.
+Define several lines (one entry per line) to be printed as label content. Template::Toolkit is supported, thus config item data can be used like "Number: [% Data.Number %]".
+
+ITSMConfigItem::Frontend::AgentITSMConfigItemLabelPrint###FooterSetting
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+Used footer settings for the label. Template::Toolkit is supported, thus config item data can be used like "Number: [% Data.Number %]".
 
 ITSMConfigItem::Frontend::AgentITSMConfigItemLabelPrint###PageSetting
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 Used page size in mm and other page settings. Be carefull if you change the size, it´s possible that the label is unusable.
-
-ITSMConfigItem::Frontend::AgentITSMConfigItemLabelPrint###Value1
-""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-It´s possible to print up to 5 CI values to the label. Please add here the CI key.
-
-ITSMConfigItem::Frontend::AgentITSMConfigItemLabelPrint###Value3
-""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-It´s possible to print up to 5 CI values to the label. Please add here the CI key.
-
-ITSMConfigItem::Frontend::AgentITSMConfigItemLabelPrint###Value5
-""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-It´s possible to print up to 5 CI values to the label. Please add here the CI key.
-
-ITSMConfigItem::Frontend::AgentITSMConfigItemLabelPrint###Value4
-""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-It´s possible to print up to 5 CI values to the label. Please add here the CI key.
 
 About
 =======
